3c01e87cae26acb3a4ffb644926fbe1c8dd7c1f1
Violeta Petkowaa <violetapetkowaa@gmail.com>
2019-09-09 15:51:33 +0200

    updated function rules

--- cologne.hs
+++ cologne.hs
@@ -38,13 +38,11 @@ cologne x = firstLetter $ lowerLetters x
 --lower the first letter in the string 
 lowerLetters :: String -> String
 lowerLetters [] = []
-lowerLetters (x:xs) =
-      if isUpper x 
-         then toLower x:xs
-         else x:xs
+lowerLetters xs = map toLower xs
 
 -- checks if first letter is 0
 firstLetter :: String -> String
+firstLetter [] = []
 firstLetter (x:xs) 
    | x == 'a' = '0' : rules xs
    | x == 'o' = '0' : rules xs
@@ -62,14 +60,23 @@ firstLetter (x:xs)
 rules :: String -> String
 rules [] = []
 rules (x:xs) 
-    | x == 'h' =  rules xs
-    | (x == 'b' || x == 'p') = '1' : rules xs
-    | (x == 'd' || x == 't') = '2' : rules xs
+    |  x == 'h' =  rules xs
+    |  x == 'b' = '1' : rules xs
     | (x == 'f' || x == 'v' || x == 'w') = '3' : rules xs
+    | (x == 'p' || x == 'c' || x == 'd' || x == 't' ) && ( rules xs == []) = rules xs -- checks if this is the last char otherwise exception is thrown
+    | (x == 'p' && head xs == 'h') = '3' : rules xs
+    |  x == 'p' = '1' : rules xs
     | (x == 'g' || x == 'k' || x == 'q') = '4' : rules xs
-    | x == 'l' = '5' : rules xs
+    | (x == 'c' && (head xs == 'a' || head xs == 'h' || head xs == 'k' || head xs == 'o' || head xs == 'q' || head xs == 'u' || head xs == 'x')) = '4' : rules xs
+    |  x == 'l' = '5' : rules xs
     | (x == 'm'|| x == 'n') = '6' : rules xs
-    | x == 'r' = '7' : rules xs
-    | ( x == 's' || x == 'z') = '8' : rules xs
-    | (x == 'a' || x == 'o' || x == 'e' || x == 'i' || x == 'u' || x == 'y' || x == 'j' || x == 'ö' || x == 'ä' || x == 'ü' || x =='ß') = '0' : rules xs
-    | otherwise = "error or not implemented yet"
+    |  x == 'r' = '7' : rules xs
+  --  | (x == 's' || x == 'z') && (head xs == 'c') = '8' : '8' : rules xs 
+    | (x == 's' || x == 'z') = '8' : rules xs
+  --  | (x == 's' || x == 'z') && (head xs == 'c') = '8' : '8' : rules xs
+    | (x == 'd' && (head xs == 'c' || head xs == 's' || head xs == 'z')) = '8' : rules xs 
+    | (x == 't' && (head xs == 'c' || head xs == 's' || head xs == 'z')) = '8' : rules xs
+    | (x == 'd' || x == 't') = '2' : rules xs
+    | (x == 'a' || x == 'o' || x == 'e' || x == 'i' || x == 'u' || x == 'y' || x == 'j' || x == 'ö' || x == 'ä' || x == 'ü' || x =='ß') = rules xs
+    | x == 'x' = '4' : '8' : rules xs
+    | otherwise = rules xs
